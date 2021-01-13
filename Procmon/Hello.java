6acaf2191ac0b518b9b4337bba946e2dc3957b95
Rush-xz <rushikesh.godase99@gmail.com>
2021-01-13 19:08:11 +0530

    Projects In C/C++

---
+++ Procmon/Hello.java
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////
+//
+//  Print Hello World on console.
+//  
+///////////////////////////////////////////////////////////////
+import java.lang.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+/* OUTPUT :
+
+Hello, World.
+Hello, Java.
+
+*/
